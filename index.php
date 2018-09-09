e0ce969b79f186bd84bfff8892a63c9579511333
anasshofyan <shofyananasmf@gmail.com>
2018-09-09 22:29:47 +0700

    commit pertama

---
+++ index.php
@@ -0,0 +1,403 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Tutor Teknik | Website Under Constructions</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="asset/css/style.css">
+</head>
+<body>
+</body>
+
+<!-- Navigation-Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light box-shadow fixed-top" style="z-index: 9999;">
+	<a class="navbar-brand" href="#"><img src="asset/img/logo.png" width="200" class="mb-1" alt=""></a>
+	<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="navbar-collapse collapse" id="navbarsExample09" style="">
+		<!-- drowdown -->
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item dropdown">
+				<a class="text-gray nav-link" href="#" id="dropdown09" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="fa fa-th mr-2"></span>Kategori</a>
+				<div class="dropdown-menu box " aria-labelledby="dropdown09">
+					<a class="dropdown-item" href="#"><span class="fa fa-desktop mr-3 text-gray"></span>Software</a>
+					<a class="dropdown-item" href="#"><span class="fa fa-microchip mr-3 text-gray"></span>Micro Controller</a>
+					<a class="dropdown-item" href="#"><span class="fa fa-bolt mr-4 text-gray"></span>Elektronika</a>
+				</div>
+			</li>
+		</ul>
+		<!-- end drowdown -->
+		<!-- menu right -->
+		<a class="text-gray nav-link " href="#"><span class="fa fa-diamond mr-2"></span>Course</a>
+		<a class="text-gray nav-link" href="#"><span class="fa fa-graduation-cap mr-2 "></span>Teach on Tutor</a>
+		<a class="text-gray nav-link" href="#"><span class="fa fa-user mr-2"></span>Login</a>
+		<!-- end menu right -->
+	</ul>
+</div>
+</nav>
+<!-- END Navigation-Bar -->
+
+<!-- Banner -->
+<header class="masthead text-white text-center">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-9  col-md-9  mx-auto">
+				<p class="font-size">Easy to Learn</p>
+				<p class="font-size mb-5">Technique Solutions</p>
+			</div>
+			<div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+				<form>
+					<div class="form-row justify-content-center">
+						<div class="col-8 col-sm-9 col-md-9 mb-2 mb-md-0">
+							<input type="text" class="form-control form-control-lg" placeholder="What will you learn today?">
+						</div>
+						<div class="">
+							<button type="submit" class="btn btn-lg btn-block btn-warning"><span class="fa fa-search text-white"></span></button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</header>
+<!-- END Banner -->
+
+<!-- Course -->
+<section>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+				<h1 style="font-size: 60px;" class="text-center mt-5 mb-5">Course</h1>
+			</div>
+			<div class="col-12">
+				<div class="container">
+					<!-- row -->
+					<div class="row justify-content-center">
+						<!-- collum 1 -->
+						<div class="col-md-3">
+							<div class="box-shadow-product mb-4">
+								<span class="badge badge-success badge-style">Intermediate</span>
+								<span class="fa fa-clock-o clock-style"><small class="text"> 2h 22m</small></span>
+								<a class="product-btn-wishlist" href="#">
+									<i class="fa fa-heart fa-heart-q mt-auto mb-auto" data-toggle="tooltip" data-placement="bottom" title="Add to Wishlist"></i>
+								</a>
+								<!-- link details -->
+								<a href="#">
+									<img class="product-img" src="asset/img/bg-banner.jpg" >
+								</a>
+								<div class="product-body">
+									<!-- tutor -->
+									<p class="product-tutor">Justin Apelible</p>
+									<!-- title product -->
+									<h6 class="product-title">Belajar Mengkontrol Wi-fi Menggunakan Ethernet Shile</h6>
+									<!-- discription -->
+									<h6 class="product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eum, esse? Animi reprehenderit aliquam, sed voluptatibus explicabo amet expedita ipsam molestiae doloremque repellat. Est atque, ut illo dicta omnis explicabo.</h6>
+									<!-- Star Rating -->
+									<div class="star-rating">
+										<div class="stars-container">
+											<div class="stars-active" style="width: 98%;">
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star-half-o text-warning"></i>
+												<span class="score">4.5</span> <span class="total-votes">(1)</span>	
+											</div> 
+										</div>
+									</div>
+									<hr>
+									<!-- price -->
+									<div href="#" class="product-price"><strong>IDR</strong>400K</div>
+									<!-- Location -->
+									<small class="product-location" style="font-size: 12px;"><span class="fa fa-location-arrow"></span> Gubeng, Surabaya</small>
+								</div>
+							</div>
+						</div>
+						<!-- end collum 1-->
+						<!-- collum 1 -->
+						<div class="col-md-3">
+							<div class="box-shadow-product mb-4">
+								<span class="badge badge-success badge-style">Intermediate</span>
+								<span class="fa fa-clock-o clock-style"><small class="text"> 2h 22m</small></span>
+								<a class="product-btn-wishlist" href="#">
+									<i class="fa fa-heart fa-heart-q mt-auto mb-auto" data-toggle="tooltip" data-placement="bottom" title="Add to Wishlist"></i>
+								</a>
+								<!-- link details -->
+								<a href="#">
+									<img class="product-img" src="asset/img/bg-banner.jpg" >
+								</a>
+								<div class="product-body">
+									<!-- tutor -->
+									<p class="product-tutor">Justin Apelible</p>
+									<!-- title product -->
+									<h6 class="product-title">Belajar Mengkontrol Wi-fi Menggunakan Ethernet Shile</h6>
+									<!-- discription -->
+									<h6 class="product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eum, esse? Animi reprehenderit aliquam, sed voluptatibus explicabo amet expedita ipsam molestiae doloremque repellat. Est atque, ut illo dicta omnis explicabo.</h6>
+									<!-- Star Rating -->
+									<div class="star-rating">
+										<div class="stars-container">
+											<div class="stars-active" style="width: 98%;">
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star-half-o text-warning"></i>
+												<span class="score">4.5</span> <span class="total-votes">(1)</span>	
+											</div> 
+										</div>
+									</div>
+									<hr>
+									<!-- price -->
+									<div href="#" class="product-price"><strong>IDR</strong>400K</div>
+									<!-- Location -->
+									<small class="product-location" style="font-size: 12px;"><span class="fa fa-location-arrow"></span> Gubeng, Surabaya</small>
+								</div>
+							</div>
+						</div>
+						<!-- end collum 1--><!-- collum 1 -->
+						<div class="col-md-3">
+							<div class="box-shadow-product mb-4">
+								<span class="badge badge-success badge-style">Intermediate</span>
+								<span class="fa fa-clock-o clock-style"><small class="text"> 2h 22m</small></span>
+								<a class="product-btn-wishlist" href="#">
+									<i class="fa fa-heart fa-heart-q mt-auto mb-auto" data-toggle="tooltip" data-placement="bottom" title="Add to Wishlist"></i>
+								</a>
+								<!-- link details -->
+								<a href="#">
+									<img class="product-img" src="asset/img/bg-banner.jpg" >
+								</a>
+								<div class="product-body">
+									<!-- tutor -->
+									<p class="product-tutor">Justin Apelible</p>
+									<!-- title product -->
+									<h6 class="product-title">Belajar Mengkontrol Wi-fi Menggunakan Ethernet Shile</h6>
+									<!-- discription -->
+									<h6 class="product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eum, esse? Animi reprehenderit aliquam, sed voluptatibus explicabo amet expedita ipsam molestiae doloremque repellat. Est atque, ut illo dicta omnis explicabo.</h6>
+									<!-- Star Rating -->
+									<div class="star-rating">
+										<div class="stars-container">
+											<div class="stars-active" style="width: 98%;">
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star-half-o text-warning"></i>
+												<span class="score">4.5</span> <span class="total-votes">(1)</span>	
+											</div> 
+										</div>
+									</div>
+									<hr>
+									<!-- price -->
+									<div href="#" class="product-price"><strong>IDR</strong>400K</div>
+									<!-- Location -->
+									<small class="product-location" style="font-size: 12px;"><span class="fa fa-location-arrow"></span> Gubeng, Surabaya</small>
+								</div>
+							</div>
+						</div>
+						<!-- end collum 1--><!-- collum 1 -->
+						<div class="col-md-3">
+							<div class="box-shadow-product mb-4">
+								<span class="badge badge-success badge-style">Intermediate</span>
+								<span class="fa fa-clock-o clock-style"><small class="text"> 2h 22m</small></span>
+								<a class="product-btn-wishlist" href="#">
+									<i class="fa fa-heart fa-heart-q mt-auto mb-auto" data-toggle="tooltip" data-placement="bottom" title="Add to Wishlist"></i>
+								</a>
+								<!-- link details -->
+								<a href="#">
+									<img class="product-img" src="asset/img/bg-banner.jpg" >
+								</a>
+								<div class="product-body">
+									<!-- tutor -->
+									<p class="product-tutor">Justin Apelible</p>
+									<!-- title product -->
+									<h6 class="product-title">Belajar Mengkontrol Wi-fi Menggunakan Ethernet Shile</h6>
+									<!-- discription -->
+									<h6 class="product-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem eum, esse? Animi reprehenderit aliquam, sed voluptatibus explicabo amet expedita ipsam molestiae doloremque repellat. Est atque, ut illo dicta omnis explicabo.</h6>
+									<!-- Star Rating -->
+									<div class="star-rating">
+										<div class="stars-container">
+											<div class="stars-active" style="width: 98%;">
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star text-warning"></i>
+												<i class="fa fa-star-half-o text-warning"></i>
+												<span class="score">4.5</span> <span class="total-votes">(1)</span>	
+											</div> 
+										</div>
+									</div>
+									<hr>
+									<!-- price -->
+									<div href="#" class="product-price"><strong>IDR</strong>400K</div>
+									<!-- Location -->
+									<small class="product-location" style="font-size: 12px;"><span class="fa fa-location-arrow"></span> Gubeng, Surabaya</small>
+								</div>
+							</div>
+						</div>
+						<!-- end collum 1-->
+					</div>
+				</div>
+				<!-- end row -->
+				<!-- button see all course -->
+				<div class="ontainer">
+					<div class="row justify-content-center">
+						<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+							<button class="btn btn-secondary button mt-3 mb-3 btn-lg">See All Course</button>
+						</div>
+					</div>
+				</div>
+				<!-- end button see all course -->
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END Course -->
+
+<!-- INTERACTION PRIVATE AND GRUB -->
+<section class="container-fluid bg-light mt-3">
+	<div class="container">
+		<!-- row -->
+		<div class="row justify-content-center">
+			<!-- col -->
+			<div class="col-12 col-sm-10 col-md-10 col-lg-5">
+				<div class="card-box mt-5 mb-5">
+					<!-- icon	 -->
+					<div class="text-center">
+						<span class="fa fa-user icon mt-3"></span>
+					</div>
+					<!-- end icon	 -->
+					<!-- title -->
+					<h1 class="text-1 mt-2 text-center">Private</h1>
+					<!-- end title -->
+					<!-- meaning -->
+					<small class="text-2">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque nam aliquid voluptatibus unde commodi, doloremque eligendi sint recusandae facere necessitatibus tempora tenetur eaque repudiandae quam, dignissimos! Aspernatur a rem facere omnis! Repellendus neque non, dicta dolorum dolore quaerat a illum eveniet inventore totam nesciunt rerum provident numquam ratione incidunt enim laborum iure eos unde maiores minus, blanditiis beatae. Consectetur, odio!
+					</small>
+					<!-- end meaning -->
+					<!-- button -->
+					<div class="row justify-content-center">
+						<div class="col-md-12 text-center">
+							<button class="btn btn-secondary button mt-3 mb-3 btn-lg ">Start Learning Now</button>
+						</div>
+					</div>
+					<!-- end button -->
+				</div>
+			</div>
+			<!-- end col -->
+			<!-- col -->
+			<div class="col-12 col-sm-10 col-md-10 col-lg-5">
+				<div class="card-box mt-5 mb-5">
+					<!-- icon	 -->
+					<div class="text-center">
+						<span class="fa fa-users icon mt-3"></span>
+					</div>
+					<!-- end icon	 -->
+					<!-- title -->
+					<h1 class="text-1 mt-2 text-center">Group</h1>
+					<!-- end title -->
+					<!-- meaning -->
+					<small class="text-2">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque nam aliquid voluptatibus unde commodi, doloremque eligendi sint recusandae facere necessitatibus tempora tenetur eaque repudiandae quam, dignissimos! Aspernatur a rem facere omnis! Repellendus neque non, dicta dolorum dolore quaerat a illum eveniet inventore totam nesciunt rerum provident numquam ratione incidunt enim laborum iure eos unde maiores minus, blanditiis beatae. Consectetur, odio!
+					</small>
+					<!-- end meaning -->
+					<!-- button -->
+					<div class="row justify-content-center">
+						<div class="col-md-12 text-center">
+							<button class="btn btn-secondary button mt-3 mb-3 btn-lg ">Start Learning Now</button>
+						</div>
+					</div>
+					<!-- end button -->
+				</div>
+			</div>
+			<!-- end col -->
+		</div>
+		<!-- end row -->
+	</div>
+</section>
+<!-- END INTERACTION PRIVATE AND GRUB -->
+
+<!-- MEDIA PARTNERS -->
+<section>
+	<div class="container-fluid">
+		<div class="container mt-5">
+			<div class="row">
+				<!-- title -->
+				<div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+					<h1 style="font-size: 60px;" class="text-center mt-5 mb-5">Partner</h1>
+				</div>
+				<!-- title -->
+			</div>
+			<div class="row">
+				<div class="col-12 col-md-4">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- END MEDIA PARTNERS -->
+
+
+<!-- FOOTER -->
+<section>
+	<div class="container-fluid bg-black-a p-5">
+		<div class="container text-white">
+			<div class="row justify-content-center text-center">
+				<!-- link course -->
+				<div class="col-md-3 mt-3">
+					<h4>Course</h4>
+					<div><a href="#" class="text-gr">Video Trailler</a></div>
+					<div><a href="#" class="text-gr">Training</a></div>
+					<div><a href="#" class="text-gr">Theroy</a></div>
+					<div><a href="#" class="text-gr">Practice</a></div>
+					<div><a href="#" class="text-gr">Direct Interaction</a></div>
+				</div>
+				<!-- end link course -->
+				<!-- link legal -->
+				<div class="col-md-3 mt-3">
+					<h4>Legal</h4>
+					<div><a href="#" class="text-gr">Policies</a></div>
+					<div><a href="#" class="text-gr">Privacy Policy</a></div>
+					<div><a href="#" class="text-gr">Cookie Policy</a></div>
+					<div><a href="#" class="text-gr">Term of Service</a></div>
+				</div>
+				<!-- end link Legal -->
+				<!-- link legal -->
+				<div class="col-md-3 mt-3">
+					<h4>Company</h4>
+					<div><a href="#" class="text-gr">About</a></div>
+					<div><a href="#" class="text-gr">Contact Us</a></div>
+					<div><a href="#" class="text-gr">Support</a></div>
+					<div><a href="#" class="text-gr">Store</a></div>
+					<div><a href="#" class="text-gr">Blog</a></div>
+				</div>
+				<!-- end link Legal -->
+			</div>
+		</div>
+	</div>
+	<!-- copyright -->
+	<div class="container-fluid line bg-black-a p-3">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<img src="./asset/img/beta.png" width="150px" alt="">
+				</div>
+				<div class="col-md-4 mt-2">
+					<small class="text-white">
+					Copyright © 2018 <a href="#" class="text-gr">TutorTeknik™</a> All rights reserved.</small>	
+				</div>
+			</div>
+		</div>
+	</div>			
+	<!-- end copyright -->
+</section>
+<!-- END FOOTER -->
+</body>
+</html>
